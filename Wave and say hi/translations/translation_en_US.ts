03aac1a68ba103980dd69de11aa96153048512c7
Nicolas Henrotte <nicolas.henrotte@student.odisee.be>
2018-11-09 11:41:00 +0100

    Tutorial + oefenprogramma's

---
+++ Wave and say hi/translations/translation_en_US.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Dag meneer Schockaert</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Dag meneer Schockaert</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hallo!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hallo!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hallo Thomas</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hallo Thomas</translation>
+        </message>
+    </context>
+</TS>
