9e7895ead3bb7836821abc90aef7eb775a254170
Nicolas Henrotte <nicolas.henrotte@student.odisee.be>
2018-11-20 19:49:14 +0100

    Updated tutorial

---
+++ Infodag/translations/translation_en_US.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Wil jij mijn vriendje worden op facebook?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Wil jij mijn vriendje worden op facebook?</translation>
+        </message>
+        <message>
+            <source>Wil jij mijn vriendje worden op feejsboek?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Wil jij mijn vriendje worden op feejsboek?</translation>
+        </message>
+        <message>
+            <source>Hallo, ik ben Pepper. Raak mijn hoofd aan</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hallo, ik ben Pepper. Raak mijn hoofd aan</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Basis Infodag/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Wil jij mijn vriendje worden op feejsboek?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Wil jij mijn vriendje worden op feejsboek?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Basis Infodag/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Scan dan deze Q.R. code</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Scan dan deze Q.R. code</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Wil jij mijn vriendje worden op feejsboek?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Wil jij mijn vriendje worden op feejsboek?</translation>
+        </message>
+        <message>
+            <source>I recognised something!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I recognised something!</translation>
+        </message>
+    </context>
+</TS>
